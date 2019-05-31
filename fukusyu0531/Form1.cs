f3d9640dfb00a80ca2919d4040f1189566c09fdb
pizza_girl <pizza.girl.dat@gmail.com>
2019-05-31 13:35:19 +0900

    完成

--- fukusyu0531/Form1.cs
+++ fukusyu0531/Form1.cs
@@ -12,9 +12,43 @@ namespace fukusyu0531
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+            if (label1.Left < 0)
+            {
+                vx = -vx;
+            }
+            if (label1.Top < 0)
+            {
+                vy = -vy;
+            }
+            if (label1.Right > 283)
+            {
+                vx = -vx;
+            }
+            if (label1.Bottom >265)
+            {
+                vy = -vy;
+            }
+        }
+                 private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("" + label1.Bottom);
+        }
+    
     }
 }
